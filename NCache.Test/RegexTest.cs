a2d5220f92514c90df612d46f8c3bacd2fb60307
softlgl <ruanjian_liang@126.com>
2021-09-24 13:22:56 +0800

    添加distributed支持

---
+++ NCache.Test/RegexTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using Xunit;
+
+namespace NCache.Test
+{
+    public class RegexTest
+    {
+        [Fact]
+        public void TestMatch()
+        {
+            Regex rgx = new Regex(@"(?i)(?<=\[)(.*)(?=\])");//中括号[]
+            string str = "person[0]";
+            string tmp = rgx.Match(str).Value;//中括号[]
+            System.Diagnostics.Debug.Write(tmp);
+            Assert.Equal("0", tmp);
+        }
+
+        [Fact]
+        public void TestMatchFilter()
+        {
+            string pattern = "person[0][1]";
+            pattern = Regex.Replace(pattern, @"\[.*\]", "");//过滤[]
+            System.Diagnostics.Debug.Write(pattern);
+            Assert.Equal("person", pattern);
+        }
+    }
+}
